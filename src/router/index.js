105e532a545705e7fb0ea3d6b94e426efea66d4e
zhoulei <403907871@qq.com>
2021-06-08 17:57:43 +0800

    - finish 3-2

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,15 @@ const routes = [
   }, {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    beforeEnter (to, form, next) {
+      const isLogin = localStorage.isLogin
+      if (isLogin) {
+        next({ name: 'Home' })
+      } else {
+        next()
+      }
+    }
   }
   // {
   //   path: '/about',
@@ -26,5 +34,13 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.isLogin
 
+  if (isLogin || to.name === 'Login') {
+    next()
+  } else {
+    next({ name: 'Login' })
+  }
+})
 export default router
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -22,7 +22,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 .wrapper {
   position: absolute;
